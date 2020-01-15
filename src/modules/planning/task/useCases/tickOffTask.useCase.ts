317d279b70d7d7c75c3e1be1e495763490b2a361
Christian Wlatschiha <christian.wlatschiha@quantilope.com>
2020-01-15 19:01:10 +0100

    feat: Tick off task.

---
+++ src/modules/planning/task/useCases/tickOffTask.useCase.ts
@@ -0,0 +1,42 @@
+import { UniqueEntityID, UseCase } from '../../../../core/domain';
+import { TickOffTaskDTO } from '../../../../graphql.schema';
+import {
+  Either,
+  eitherLeft,
+  eitherRight,
+  GenericAppError,
+  Result,
+} from '../../../../core/logic';
+import { TaskEntity, TaskId } from '../domain';
+import { Injectable } from '@nestjs/common';
+import { TaskRepository } from '../task.repository';
+import { TickOffTaskErrors } from './tickOffTask.errors';
+
+type Response = Either<
+  TickOffTaskErrors.TaskDoesNotExist | GenericAppError.UnexpectedError,
+  Result<TaskEntity>
+>;
+
+@Injectable()
+export class TickOffTaskUseCase implements UseCase<TickOffTaskDTO, Response> {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  public async execute(dto: TickOffTaskDTO): Promise<Response> {
+    const taskId = TaskId.create(UniqueEntityID.create(dto.taskId));
+    try {
+      const [found, taskEntity] = await this.taskRepository.findByTaskId(
+        taskId,
+      );
+      if (!found) {
+        return eitherLeft(
+          new TickOffTaskErrors.TaskDoesNotExist(taskId),
+        ) as Response;
+      }
+      taskEntity.tickOff();
+      await this.taskRepository.save(taskEntity);
+      return eitherRight(Result.ok(taskEntity)) as Response;
+    } catch (error) {
+      return eitherLeft(new GenericAppError.UnexpectedError(error)) as Response;
+    }
+  }
+}
